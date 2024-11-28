8d9cedc55f640e75d1162d4baf2fa00455479187
Shishir Adhikari <adhikarishishir11@gmail.com>
2024-11-28 15:29:05 +0545

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,17 @@
+print("enter the operator")
+#operators are +,-,*,/
+operator=input()
+print("enter the first number")
+num1=int(input())
+print("enter the second number")
+num2=int(input())
+if operator=="+":
+    print(num1+num2)
+elif operator=="-":
+    print(num1-num2)
+elif operator=="*":
+    print(num1*num2)
+elif operator=="/":
+    print(num1/num2)    
+else:
+    print("invalid operator")
